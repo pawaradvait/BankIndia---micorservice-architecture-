76c914693dd9c4e6689f3055bc6879c55474e47b
advait pawar <pawaradvait2003@gmail.com>
2024-10-25 10:52:37 +0530

    bankIndia -v1 refresh config at runtime using  refresh actuator

--- account/src/main/java/com/bankIndia/account/dto/AccountContactInfoDto.java
+++ account/src/main/java/com/bankIndia/account/dto/AccountContactInfoDto.java
@@ -4,7 +4,14 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import lombok.Data;
 @ConfigurationProperties(prefix = "account")
-public record AccountContactInfoDto(String message,Map<String,String> contactDetails,List<String> onCallSupport) {
+@Data
+public class AccountContactInfoDto {
+
+   private String message;
+    private Map<String,String> contactDetails;
+   private  List<String> onCallSupport;
 
 }
