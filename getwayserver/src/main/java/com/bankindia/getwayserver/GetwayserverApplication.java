b0b622956f2493d1c9afd0645ca35f9216ef422f
advait pawar <pawaradvait2003@gmail.com>
2024-10-29 15:03:34 +0530

    bankindia -v1 -> implement gatewayserver

---
+++ getwayserver/src/main/java/com/bankindia/getwayserver/GetwayserverApplication.java
@@ -0,0 +1,13 @@
+package com.bankindia.getwayserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetwayserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetwayserverApplication.class, args);
+	}
+
+}
---
+++ getwayserver/src/test/java/com/bankindia/getwayserver/GetwayserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bankindia.getwayserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetwayserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
