a20ab78ca533d2d9208d5f7ffa6330abd6440ba6
advait pawar <pawaradvait2003@gmail.com>
2024-11-01 13:28:54 +0530

    bankindia -v1 -> implemented rate limiter in gateway server for loan Api's

--- account/src/main/java/com/bankIndia/account/AccountApplication.java
+++ account/src/main/java/com/bankIndia/account/AccountApplication.java
@@ -1,10 +1,15 @@
 package com.bankIndia.account;
 
+import java.time.Duration;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.client.RestTemplate;
 
 import com.bankIndia.account.dto.AccountContactInfoDto;
 
--- getwayserver/src/main/java/com/bankindia/getwayserver/GetwayserverApplication.java
+++ getwayserver/src/main/java/com/bankindia/getwayserver/GetwayserverApplication.java
@@ -5,10 +5,19 @@ import java.time.LocalDateTime;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.circuitbreaker.resilience4j.ReactiveResilience4JCircuitBreakerFactory;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
+import org.springframework.cloud.client.circuitbreaker.Customizer;
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.cloud.gateway.filter.ratelimit.RedisRateLimiter;
 import org.springframework.cloud.gateway.route.Route;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
+
+import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
+import io.github.resilience4j.timelimiter.TimeLimiterConfig;
+import reactor.core.publisher.Mono;
 
 @SpringBootApplication
 public class GetwayserverApplication {
@@ -34,8 +43,31 @@ public class GetwayserverApplication {
  .filters(f->f.rewritePath("/bankindia/loan/(?<segment>.*)", "/${segment}")
  .addResponseHeader("X-RESPONSE-TIME", LocalDateTime.now().toString())
  .retry(retryconfig -> retryconfig.setRetries(3).setBackoff(Duration.ofMillis(100),Duration.ofMillis(10000), 2, true))
+ .requestRateLimiter(config -> config.setRateLimiter(redisRateLimiter()).setKeyResolver(userKeyResolver()))
+ 
  )
+
  .uri("lb://LOAN")).build();
+	}
+
+
+		@Bean
+	public Customizer<ReactiveResilience4JCircuitBreakerFactory> defaultCustomizer() {
+		return factory -> factory.configureDefault(id -> new Resilience4JConfigBuilder(id)
+				.circuitBreakerConfig(CircuitBreakerConfig.ofDefaults())
+				.timeLimiterConfig(TimeLimiterConfig.custom().timeoutDuration(Duration.ofSeconds(4)).build()).build());
+	}
+
+
+	@Bean
+	public RedisRateLimiter redisRateLimiter() {
+		return new RedisRateLimiter(1, 1, 1);
+	}
+
+	@Bean
+	KeyResolver userKeyResolver() {
+		return exchange -> Mono.justOrEmpty(exchange.getRequest().getHeaders().getFirst("user"))
+				.defaultIfEmpty("anonymous");
 	}
 
 }
